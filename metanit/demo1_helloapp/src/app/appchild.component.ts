fdd14a1e3f923bf39bb05edd4f2dc6ce5185fad6
RSatanovskyi <satcms@hotmail.com>
2020-02-20 13:48:05 +0200

    ViewChild

--- metanit/demo1_helloapp/src/app/appchild.component.ts
+++ metanit/demo1_helloapp/src/app/appchild.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Child20Component} from './child20.component';
 
 @Component({
@@ -7,6 +7,11 @@ import { Child20Component} from './child20.component';
             <child20-comp></child20-comp>
             <button (click)="increment()">+</button>
             <button (click)="decrement()">-</button>
+
+            <br />Variable of template<br />
+            <p #nameText>{{name}}</p>
+               <p>{{nameText.textContent}}</p>
+               <button (click)="change()">Изменить</button>
     `,
     //https://metanit.com/web/angular2/2.9.php
     styles: [` 
@@ -14,9 +19,24 @@ import { Child20Component} from './child20.component';
     `]
 })
 export class AppChildComponent { 
+    //http://localhost:8080/index_child.html
+
     @ViewChild(Child20Component, {static: false})
     private counterComponent: Child20Component;
      
     increment() { this.counterComponent.increment(); }
     decrement() { this.counterComponent.decrement(); }
+
+
+    //https://metanit.com/web/angular2/2.9.php
+    //Привязка ViewChild к шаблонным переменным
+    @ViewChild("nameText", {static: false})
+    nameParagraph: ElementRef;
+     
+    name: string = "Tom";
+     
+    change() { 
+        console.log(this.nameParagraph.nativeElement.textContent); 
+        this.nameParagraph.nativeElement.textContent = "hell";
+    }
 }
